e2dd8288be12fbf3829bdb72a305ed346e56bc13
davidgarrett111101-del <davidgarrett111101@gmail.com>
2025-08-01 12:51:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;KNFDBDhD#vUHUSZ9R4QptFljeWDDY0PxnRNvuERqZcLX93dsjgTc", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
